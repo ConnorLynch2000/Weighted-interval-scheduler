6019b43464e2dd617eb0d526b440985cf516b1e8
Vahid Ramezani <vahid.ramezani.2014@gmail.com>
2023-02-15 12:59:31 +0330

    initial commit

---
+++ src/main/java/personal/vahid/schedulerengine/SchedulerEngineApplication.java
@@ -0,0 +1,13 @@
+package personal.vahid.schedulerengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerEngineApplication.class, args);
+    }
+
+}

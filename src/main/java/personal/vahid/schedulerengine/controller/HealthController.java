74121e12e81255aaae8b971cd66d506e37700d09
Vahid Ramezani <vahid.ramezani.2014@gmail.com>
2023-02-16 21:00:38 +0330

    added health check

---
+++ src/main/java/personal/vahid/schedulerengine/controller/HealthController.java
@@ -0,0 +1,20 @@
+package personal.vahid.schedulerengine.controller;
+
+import jakarta.persistence.EntityManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    EntityManager entityManager;
+
+    @GetMapping(produces = "application/json")
+    public Boolean health(){
+        return (Boolean) entityManager.createNativeQuery("SELECT true")
+                .getSingleResult();
+    }
+}
